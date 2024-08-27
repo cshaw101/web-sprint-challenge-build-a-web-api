5b34a61ec7b8390080eaa6ebc8e083be990d664d
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-27 12:38:17 -0400

    first get router is working

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,33 @@
 // Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model')
+
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    const projects = await Projects.get()
+    
+    try {
+        if (!projects) {
+            return res.json({})
+        }else {
+            return res.json(projects)
+        }
+    }catch (err) {
+        res.status(404).json({message:'GET /projects/ isnt working!'})
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const server = express();
-
+const projectsRouter = require('./projects/projects-router')
 // Configure your server here
 server.use(express.json());
+server.use('/projects', projectsRouter)
 
 server.get('/', (req, res) => {
     res.send('Hello, im Working!');
 });
+
+
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: 'this does not exist you messed it up' });
+  });
 
 
 // Build your actions router in /api/actions/actions-router.js
