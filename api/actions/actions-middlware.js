3812a2d4382912719a591c5cbcebe7bd0ef931a1
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-27 15:02:38 -0400

    first two action endpoints working

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,22 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model')
+
+async function validateUserId(req, res, next) {
+    try {
+        const { id } = req.params;
+        const action = await Actions.get(id);
+  
+        if (action) {
+            req.action = action; 
+            next();
+        } else {
+            return res.status(404).json({ message: "action not found" }); 
+        }
+    } catch (err) {
+        return res.status(500).json({ message: "Error retrieving action", error: err.message }); 
+    }
+}
+
+
+
+  module.exports = {validateUserId}
