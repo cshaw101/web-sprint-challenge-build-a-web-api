779769e63db0a87c09f06dd4b522c8d55759ffe0
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-27 15:02:23 -0400

    first two action endpoints completed and working

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,36 @@
 // Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model')
+const { validateUserId } = require('./actions-middlware')
+
+
+const router = express.Router()
+
+
+router.get('/', async (req, res) => {
+    const actions = await Actions.get()
+
+    try{
+        if (!actions) {
+            return res.status(404).json({})
+        }else {
+            return res.status(200).json(actions)
+        }
+    }catch(err){
+        res.status(404).json({message:'GET /actions/ isnt working!'})
+    }
+})
+
+router.get('/:id', validateUserId, async (req, res) => {
+    res.status(200).json(req.action);
+})
+
+
+
+
+
+
+
+
+
+module.exports = router
